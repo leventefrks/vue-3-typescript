3cf0fea7cf65c880889ea62586f67096b8b29ef0
x-farkasle <x-farkasle@wizzair.com>
2021-03-11 19:21:46 +0100

    EventItem interface

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface EventItem {
+  id: number
+  category: string
+  title: string
+  description: string
+  location: string
+  date: 'January 22, 2022'
+  time: '12:00'
+  organizer: string
+}
